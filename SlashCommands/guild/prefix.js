5869e4a40c79dd3323b42ff436cde54f3223c2ca
Prabu12345 <35483546+Prabu12345@users.noreply.github.com>
2021-09-06 07:53:35 +0700

    sdad

--- SlashCommands/guild/prefix.js
+++ SlashCommands/guild/prefix.js
@@ -1,8 +1,19 @@
 const { Client, Interaction } = require("discord.js");
+const messageCreate = require("../../events/guild/messageCreate");
+const guildSchema = require("../../resource/database/guild")
 
 module.exports = {
   name: "prefix",
-  description: "change bot prefix",
+  description: "Change bot prefix",
+  type: "CHAT_INPUT",
+  options: [
+    {
+      name: "prefix",
+      description: "what change to?",
+      type: 3,
+      required: true,
+    },
+  ],
   /**
    *
    * @param {Client} client
@@ -11,7 +22,18 @@ module.exports = {
   // just for telling that u can also add options
   execute: async (client, interaction) => {
     try {
-      interaction.reply({content: "Prefix Command is Not Ready Yet"});
+      const newPrefix = interaction.options._hoistedOptions.find((f) => f.name === "text").value;
+      
+      await guildSchema.findOneAndUpdate(
+        {
+            id: interaction.guild.id
+        },
+        {
+            prefix: newPrefix
+        }
+      )
+
+      interaction.reply({content: `<@${interaction.author.id}>, Set the command prefix to \`${newPrefix}\`. To run commands, use \`${newPrefix}command\`.`})
     } catch (err) {
       console.log("Something Went Wrong => ",err);
     }
